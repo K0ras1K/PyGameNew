cb4d17cb2e5997f85b30f2a760ea8e2eff718d6e
K0ras1K <78379848+K0ras1K@users.noreply.github.com>
2023-01-16 23:56:27 +0300

    Update PyGame.py
    fix logic

--- PyGame.py
+++ PyGame.py
@@ -10,30 +10,31 @@ class Game:
         self.activatedBomb = (-1, -1)
         #TODO При gameOver = True вызвать окно окончания игры
         self.gameOver = False
+        self.progress = 1
 
     #TODO Вызывать по нажатию лкм по квадратику поля
     def movePlayer(self, xCoord, yCoord):
         if self.canMove(xCoord, yCoord):
-            self.mainBoard.mainBoardNetz[self.player.xCoord][self.player.yCoord] = 2
-            self.player.move(xCoord, yCoord)
-            self.mainBoard.mainBoardNetz[xCoord][yCoord] = 3
+            self.mainBoard.mainBoardNetz[self.mainBoard.getPlayerCoords()[0]][self.mainBoard.getPlayerCoords()[1]], self.mainBoard.mainBoardNetz[xCoord][yCoord] = 2, 3
             self.mainBoard.printNetz()
             print()
-            if self.redBomb.checkPlayer(xCoord, yCoord, self.mainBoard.getBombCoords()) != (-1, -1):
-                self.activatedBomb = (self.redBomb.checkPlayer(xCoord, yCoord, self.mainBoard.getBombCoords()))
-                #TODO Добавить вызывание звука активации бомбы
-                print('Звук бомбы!')
-                print()
             if self.redBomb.checkActivatedBomb(self.activatedBomb[0], self.activatedBomb[1], xCoord, yCoord):
                 self.player.bombActivating(self.bombDamage)
             else:
                 self.activatedBomb = (-1, -1)
+            if self.redBomb.checkPlayer(xCoord, yCoord, self.mainBoard.getBombCoords()) != (-1, -1):
+                self.activatedBomb = (self.redBomb.checkPlayer(xCoord, yCoord, self.mainBoard.getBombCoords()))
+                #TODO Добавить вызывание звука активации бомбы
+                print('Звук бомбы!')
+                print(self.activatedBomb)
+                print()
             if self.player.HP == 0:
                 self.gameOver = True
                 print('Игра окончена')
                 print()
+            self.progress += 1
         else:
-            print('Нельзя!')
+            print('Нельзя! move')
             print()
 
     def canMove(self, xCoord, yCoord):
@@ -44,21 +45,24 @@ class Game:
 
     #TODO Вызывать по нажатию пкм по квадратику
     def breakIce(self, xCoord, yCoord):
-        if self.player.canBreakIce(xCoord, yCoord):
-            if self.canBreakIce(xCoord, yCoord):
-                self.mainBoard.breakIce(xCoord, yCoord)
-        if self.redBomb.checkActivatedBomb(self.activatedBomb[0], self.activatedBomb[1], self.player.xCoord, self.player.yCoord):
+        if self.canBreakIce(xCoord, yCoord):
+            self.mainBoard.breakIce(xCoord, yCoord)
+        if self.redBomb.checkActivatedBomb(self.activatedBomb[0], self.activatedBomb[1], self.mainBoard.getPlayerCoords()[0], self.mainBoard.getPlayerCoords()[1]):
             self.player.bombActivating(self.bombDamage)
         else:
             self.activatedBomb = (-1, -1)
         if self.player.HP == 0:
             self.gameOver = True
+            print("Game Over")
+        self.progress += 1
         self.mainBoard.printNetz()
 
     def canBreakIce(self, xCoord, yCoord):
         if self.mainBoard.mainBoardNetz[xCoord][yCoord] != 2:
-            if self.player.canBreakIce(xCoord, yCoord):
+            if self.player.canBreakIce(xCoord, yCoord, self.mainBoard.getPlayerCoords()[0], self.mainBoard.getPlayerCoords()[1], self.progress):
                 return True
+        print("Нельзя break")
+        return False
 
 
 class MainBoard:
@@ -67,7 +71,7 @@ class MainBoard:
         self.ySize = ySize
         self.bombSpawnChance = bombSpawnChance
         self.mainBoardNetz = [[self.getBoolByChance(self.bombSpawnChance) for i in range(self.xSize)] for j in
-                              range(self.ySize)]
+                               range(self.ySize)]
         self.mainBoardNetz[0][0] = 3
         for i in self.mainBoardNetz:
             print(i)
@@ -94,6 +98,12 @@ class MainBoard:
             print(i)
         print()
 
+    def getPlayerCoords(self):
+        for i in range(len(self.mainBoardNetz)):
+            for j in range(len(self.mainBoardNetz[0])):
+                if self.mainBoardNetz[i][j] == 3:
+                    return (i, j)
+
 
 class RedBomb:
     def __init__(self, bombDamage, bombCoords):
@@ -118,8 +128,6 @@ class RedBomb:
 
 class Player:
     def __init__(self, HP):
-        self.xCoord = 0
-        self.yCoord = 0
         self.HP = HP
         self.progress = 1
 
@@ -129,18 +137,17 @@ class Player:
         else:
             self.setDeadth()
 
-    def move(self, xCoord, yCoord):
-        if self.progress % 2 != 0:
-            return
-        if abs(xCoord - self.xCoord) <= 1 and abs(yCoord - self.yCoord) <= 1:
-            self.xCoord = xCoord
-            self.yCoord = yCoord
-        self.progress += 1
-
-    def canBreakIce(self, xCoord, yCoord):
-        if self.progress % 2 != 1:
+    def canMove(self, xCoord, yCoord, lastX, lastY, progress):
+        if progress % 2 != 0:
             return False
-        if abs(xCoord - self.xCoord) <= 1 and abs(yCoord - self.yCoord) <= 1:
+        if abs(xCoord - lastX) <= 1 and abs(yCoord - lastY) <= 1:
+            return True
+        return False
+
+    def canBreakIce(self, xCoord, yCoord, lastX, lastY, progress):
+        # if progress % 2 != 1:
+        #     return False
+        if abs(xCoord - lastX) <= 1 and abs(yCoord - lastY) <= 1:
             return True
         return False
 
@@ -148,3 +155,16 @@ class Player:
         self.HP = 0
 
 
+game = Game(10, 10, 10, 3, 3)
+game.breakIce(1, 1)
+game.movePlayer(1, 1)
+game.breakIce(2, 1)
+game.movePlayer(2, 1)
+game.breakIce(3, 1)
+game.movePlayer(3, 1)
+game.breakIce(4, 1)
+game.movePlayer(4, 1)
+game.breakIce(5, 1)
+game.movePlayer(5, 1)
+game.breakIce(6, 1)
+game.movePlayer(6, 1)
